9e79151ea3c7a7e6f02ff244c6d3e21adb2162a4
voidSatisfaction <lourie@naver.com>
2016-10-26 14:13:54 +0900

    play with scheme

--- seasonedSchemer/ch14/main.scm
+++ seasonedSchemer/ch14/main.scm
@@ -4,12 +4,60 @@
 
 (define leftmost
   (lambda (l)
-    (cond ((null? l) '())
-          ((atom? (car l)) (car l))
-          (else
-           (cond ((atom? (leftmost (car l)))
-                  (leftmost (car l)))
-                 (else
-                  (leftmost (cdr l))))))))
+      (cond ((null? l) '())
+            ((atom? (car l)) (car l))
+            (else
+             (let ((a (leftmost (car l))))
+               (cond ((atom? a) a)
+                     (else (leftmost (cdr l)))))))))
 
-(leftmost '((((()) ((a)) 2) a) c))
+(define eqlist?
+  (lambda (l1 l2)
+    (cond ((and (null? l1) (null? l2)) #t)
+          ((or (null? l1) (null? l2)) #f)
+          (else
+           (cond ((eq? (car l1) (car l2)) (eqlist? (cdr l1) (cdr l2)))
+                 (else #f))))))
+
+(define rember1*
+  (lambda (a l)
+    (letrec
+        ((R (lambda (l)
+              (cond ((null? l) '())
+                    ((atom? (car l))
+                     (cond ((eq? a (car l)) (cdr l))
+                           (else (cons (car l) (R (cdr l))))))
+                    (else
+                     (let ((av (R (car l))))
+                       (cond
+                         ((eqlist? (car l) av)
+                          (cons (car l) (R (cdr l))))
+                         (else
+                          (cons av (cdr l))))))))))
+      (R l))))
+
+#| cf
+(define rember1*
+  (lambda (a l)
+    (cond ((null? l) '())
+          ((atom? (car l))
+           (cond ((eq? a (car l)) (cdr l))
+                 (else (cons (car l) (rember1* a (cdr l))))))
+          (else
+           (cons (rember1* a (car l))
+                 (rember1* a (cdr l)))))))
+|#
+
+(define depth*
+  (lambda (l)
+    (cond ((null? l) 1)
+          ((atom? (car l)) (depth* (cdr l)))
+          (else
+           (cond ((> (depth* (cdr l)) (+ (depth* (car l)) 1))
+                  (depth* (cdr l)))
+                 (else
+                  (+ (depth* (car l)) 1)))))))
+
+(leftmost '((swedish rye) (french (mustard salad turkey)) salad))
+(rember1* 'salad '((swedish rye) (french (mustard salad turkey)) salad))
+(depth* '(c (b (a b) a) a))
